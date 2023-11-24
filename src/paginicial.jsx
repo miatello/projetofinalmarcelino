0790e2cb689d984256ad968c350ea4fdbc08e5fa
Gustavo Miatello <131817165+miatello@users.noreply.github.com>
2023-11-24 17:38:27 -0400

    inicio do projeto
    adicionei o navbar e copiei o código base do moodle.

---
+++ src/paginicial.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom"
+import "../style/card.css"
+
+function Cards ({ ListaVideos }) {
+
+    return (
+
+    ListaVideos.map((video) => 
+    <div className="card">
+        <iframe
+        width="853"
+        height="480"
+        src={"https://youtu.be/zG5gWncAhls?si=z2uuT5meBgngCOnI" + video.url.slice(17)}
+        frameborder="0"
+        allow=""
+></iframe>
+</div>
+))};
+
+<div className="container">
+    <Menu/>
+    <NavBar nomeSite={"Nome do Site"} />
+    <div className="card-container">
+        <Cards ListaVideos={ListaVideos} />
+    </div>
+</div>
